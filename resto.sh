4878bb6d0ea8145caffee190631c7cf9a6cf95e1
AngIMAN <151988382+AngIMAN@users.noreply.github.com>
2024-06-29 08:12:44 +0700

    Update resto.sh

--- resto.sh
+++ resto.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+CHATID=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 3)
+KEY=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 2)
+export TIME="10"
+export URL="https://api.telegram.org/bot$KEY/sendMessage"
 clear
-echo
-    bottoken=$(sed -n '1p' /root/.bckupbot | awk '{print $1}')
-    read -p "File ID   : " fileId
-    read -p "File PATH : " filePath
-    curl -Ss --request GET \
-        --url "https://api.telegram.org/file/bot${bottoken}/${filePath}?file_id=${fileId}" >backup.zip
+function notif_restore() {
+    green "Notif AddHost Tele"
+    sleep 2
+    CHATID="$CHATID"
+KEY="$KEY"
+TIME="$TIME"
+URL="$URL"
+TEXT="
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>  ⚠️ RESTORE NOTIF⚠️</b>
+<b>     Detail Restore VPS</b>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<code>Restore Vps Done</code>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<code>BY BOT : @IlhamStore23</code>
+"
 
-    echo -e "[ ${green}INFO${NC} ] • Restore Data..."
-	read -rp "Password File: "  InputPass
-	echo -e "[ ${green}INFO${NC} ] • Getting your data..."
-	unzip -P $InputPass /root/backup.zip &> /dev/null
-    echo -e "[ ${green}INFO${NC} ] • Starting to restore data..."
-    rm -f /root/backup.zip &> /dev/null
-	sleep 1
-    cd /root/backup
-	echo -e "[ ${green}INFO${NC} ] • Please Wait , Restoring In Process Now... "
-	sleep 1
-	cp passwd /etc/ &>/dev/null
-	echo -e "[ ${green}INFO${NC} ] • Restoring passwd data..."
-	sleep 1
-	cp group /etc/ &>/dev/null
-	echo -e "[ ${green}INFO${NC} ] • Restoring group data..."
-	sleep 1
-	cp shadow /etc/ &>/dev/null
-	echo -e "[ ${green}INFO${NC} ] • Restoring shadow data..."
-	sleep 1
-	cp gshadow /etc/ &>/dev/null
-	echo -e "[ ${green}INFO${NC} ] • Restoring gshadow data..."
-	sleep 1
-    cp -r LT /var/lib/ &>/dev/null
-    cp -r xray /etc/ &>/dev/null
-    cp -r html /var/www/ &>/dev/null
-    rm -rf /root/backup
-    rm -f backup.zip
-    echo -e "[ ${green}INFO${NC} ] • Done Restore... "
-    read -n 1 -s -r -p "Press any key to back on menu"
-    menu
+curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+}
+# ==========================================
+# Getting
+clear
+echo "Silahkan Masukin Link Backupnya"
+read -rp "Link File: " -e url
+wget -O backup.zip "$url"
+unzip backup.zip
+rm -f backup.zip
+sleep 1
+echo Start Restore
+cd /root/backup
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp -r kyt /var/lib/
+cp -r xray /etc/
+cp -r html /var/www/
+cp crontab /etc/
+
+notif_restore
+rm -rf /root/backup
+rm -f backup.zip
+echo ""
