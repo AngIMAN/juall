f7fb16bfee36f6440ec1c20f75d843f20c5d5e6d
AngIMAN <151988382+AngIMAN@users.noreply.github.com>
2024-07-19 16:14:13 +0700

    Update m-sshovpn.sh

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -1112,6 +1112,52 @@ fi
 read -n 1 -s -r -p "Press any key to back on menu"
 m-sshovpn
 }
+function autodel(){
+clear
+hariini=`date +%d-%m-%Y`
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;41;36m               AUTO DELETE                \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "Thank you for removing the EXPIRED USERS"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds`
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+do
+username=$username" "
+done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+echo "echo "Expired- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
+echo "echo "Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
+echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
+userdel $username
+fi
+done
+echo " "
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[44;91;1m        TANILINK TUNNELING          \e[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+}
 function lockssh(){
 clear
 cd
@@ -1221,6 +1267,7 @@ case $opt in
 08 | 8) clear ; listssh ; exit ;;
 09 | 9) clear ; lockssh ; exit ;;
 10 | 10) clear ; hapuslama ; exit ;;
+11 | 11) clear ; autodel ; exit ;;
 00 | 0) clear ; menu ; exit ;;
 X  | 0) clear ; m-sshovpn ;;
 x) exit ;;
